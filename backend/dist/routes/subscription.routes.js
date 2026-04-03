01491ab1d47626a7b7ca33f38ecd44e4c7e58ff7
Ubuntu <ubuntu@ip-172-31-40-113.ap-south-1.compute.internal>
2026-04-03 13:31:37 +0000

    feat: add public /health endpoint and remove dist from version control

--- backend/dist/routes/subscription.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const subscription_controller_1 = require("../controllers/subscription.controller");
-const auth_middleware_1 = require("../middleware/auth.middleware");
-const validate_middleware_1 = require("../middleware/validate.middleware");
-const router = (0, express_1.Router)();
-router.get('/me', auth_middleware_1.requireAuth, (0, validate_middleware_1.asyncHandler)(subscription_controller_1.getMySubscriptionsController));
-exports.default = router;
